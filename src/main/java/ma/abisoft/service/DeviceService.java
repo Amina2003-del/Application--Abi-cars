11357ab52b00784055f77847bec7d3edfdc1f93c
Amina2003-del <ammina.ramii@gmail.com>
2025-11-23 01:06:41 +0000

    Ajout de GeoLite2-City.mmdb pour GeoIP

--- src/main/java/ma/abisoft/service/DeviceService.java
+++ src/main/java/ma/abisoft/service/DeviceService.java
@@ -1,10 +1,7 @@
 package ma.abisoft.service;
+
 import org.springframework.lang.Nullable;
 import com.google.common.base.Strings;
-import com.maxmind.geoip2.DatabaseReader;
-import com.maxmind.geoip2.exception.GeoIp2Exception;
-import com.maxmind.geoip2.model.CityResponse;
-
 import ma.abisoft.persistence.dao.DeviceMetadataRepository;
 import ma.abisoft.persistence.model.DeviceMetadata;
 import ma.abisoft.persistence.model.User;
@@ -14,15 +11,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 import ua_parser.Client;
 import ua_parser.Parser;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.net.InetAddress;
 import java.util.*;
 
 import static java.util.Objects.nonNull;
@@ -30,38 +24,37 @@ import static java.util.Objects.nonNull;
 @Component
 public class DeviceService {
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private MailClient mailClient;
+
     private static final String UNKNOWN = "UNKNOWN";
 
     @Value("${support.email}")
     private String from;
 
-    private DeviceMetadataRepository deviceMetadataRepository;
-    private DatabaseReader databaseReader;
-    private Parser parser;
-    private JavaMailSender mailSender;
-    private MessageSource messages;
+    private final DeviceMetadataRepository deviceMetadataRepository;
+    private final Parser parser;
+    private final JavaMailSender mailSender;
+    private final MessageSource messages;
 
+    // DatabaseReader supprimé car non utilisé
     public DeviceService(DeviceMetadataRepository deviceMetadataRepository,
-                            @Nullable DatabaseReader databaseReader,
                          Parser parser,
                          JavaMailSender mailSender,
                          MessageSource messages) {
         this.deviceMetadataRepository = deviceMetadataRepository;
-        this.databaseReader = databaseReader;
         this.parser = parser;
         this.mailSender = mailSender;
         this.messages = messages;
     }
 
-    public void verifyDevice(User user, HttpServletRequest request) throws IOException, GeoIp2Exception {
-
+    public void verifyDevice(User user, HttpServletRequest request) {
         String ip = extractIp(request);
-        System.out.println("Adresse Ip: "+ip);
-        //String location = getIpLocation(ip);
-        String location = "Frekkkkaaaaee";
+        logger.info("Adresse IP: {}", ip);
 
+        // Utilisation d'une valeur par défaut pour la localisation
+        String location = "UNKNOWN_LOCATION";
         String deviceDetails = getDeviceDetails(request.getHeader("user-agent"));
 
         DeviceMetadata existingDevice = findExistingDevice(user.getId(), deviceDetails, location);
@@ -79,20 +72,12 @@ public class DeviceService {
             existingDevice.setLastLoggedIn(new Date());
             deviceMetadataRepository.save(existingDevice);
         }
-
     }
 
     private String extractIp(HttpServletRequest request) {
-        String clientIp;
         String clientXForwardedForIp = request.getHeader("x-forwarded-for");
-        if (nonNull(clientXForwardedForIp)) {
-            clientIp = parseXForwardedHeader(clientXForwardedForIp);
-        } else {
-            clientIp = request.getRemoteAddr();
-           // clientIp = request.getLocalAddr();
-        }
-
-        return clientIp;
+        return nonNull(clientXForwardedForIp) ? parseXForwardedHeader(clientXForwardedForIp)
+                                              : request.getRemoteAddr();
     }
 
     private String parseXForwardedHeader(String header) {
@@ -100,83 +85,34 @@ public class DeviceService {
     }
 
     private String getDeviceDetails(String userAgent) {
-        String deviceDetails = UNKNOWN;
+        if (userAgent == null) return UNKNOWN;
 
         Client client = parser.parse(userAgent);
-        if (Objects.nonNull(client)) {
-            deviceDetails = client.userAgent.family + " " + client.userAgent.major + "." + client.userAgent.minor +
-                    " - " + client.os.family + " " + client.os.major + "." + client.os.minor;
+        if (nonNull(client)) {
+            return client.userAgent.family + " " + client.userAgent.major + "." + client.userAgent.minor +
+                   " - " + client.os.family + " " + client.os.major + "." + client.os.minor;
         }
-
-        return deviceDetails;
-    }
-
-    private String getIpLocation(String ip) throws IOException, GeoIp2Exception {
-
-        String location = UNKNOWN;
-
-        InetAddress ipAddress = InetAddress.getByName(ip);
-
-        CityResponse cityResponse = databaseReader.city(ipAddress);
-        if (Objects.nonNull(cityResponse) &&
-                Objects.nonNull(cityResponse.getCity()) &&
-                !Strings.isNullOrEmpty(cityResponse.getCity().getName())) {
-
-            location = cityResponse.getCity().getName();
-        }
-
-        return location;
+        return UNKNOWN;
     }
 
     private DeviceMetadata findExistingDevice(Long userId, String deviceDetails, String location) {
-
-        List<DeviceMetadata> knownDevices = deviceMetadataRepository.findByUserId(userId);
-
-        for (DeviceMetadata existingDevice : knownDevices) {
-            if (existingDevice.getDeviceDetails().equals(deviceDetails) &&
-                    existingDevice.getLocation().equals(location)) {
-                return existingDevice;
-            }
-        }
-
-        return null;
+        return deviceMetadataRepository.findByUserId(userId)
+                .stream()
+                .filter(d -> d.getDeviceDetails().equals(deviceDetails) && d.getLocation().equals(location))
+                .findFirst()
+                .orElse(null);
     }
 
     private void unknownDeviceNotification(String deviceDetails, String location, String ip, String email, Locale locale) {
         final String subject = "New Login Notification";
-     /*   final SimpleMailMessage notification = new SimpleMailMessage();
-        notification.setTo(email);
-        notification.setSubject(subject);
+        String text = messages.getMessage("message.login.notification.deviceDetails", null, locale) +
+                      " " + deviceDetails + "\n" +
+                      messages.getMessage("message.login.notification.location", null, locale) +
+                      " " + location + "\n" +
+                      messages.getMessage("message.login.notification.ip", null, locale) +
+                      " " + ip;
 
-        String text = messages
-                .getMessage("message.login.notification.deviceDetails", null, locale) +
-                " " + deviceDetails +
-                "\n" +
-                messages
-                        .getMessage("message.login.notification.location", null, locale) +
-                " " + location +
-                "\n" +
-                messages
-                        .getMessage("message.login.notification.ip", null, locale) +
-                " " + ip;
-        notification.setText(text);
-        notification.setFrom(from);
-
-        mailSender.send(notification);*/
-        String text = messages
-                .getMessage("message.login.notification.deviceDetails", null, locale) +
-                " " + deviceDetails +
-                "\n" +
-                messages
-                        .getMessage("message.login.notification.location", null, locale) +
-                " " + location +
-                "\n" +
-                messages
-                        .getMessage("message.login.notification.ip", null, locale) +
-                " " + ip;
-       // mailClient.prepareAndSend(email, text,subject);
-        
-        
+        // Envoi du mail (décommenter si nécessaire)
+        // mailClient.prepareAndSend(email, text, subject);
     }
-
 }
